a80e11dc5b1d95c901d48027d448e3015e400353
Melanie Corchado <melaniecorchado@melanies-mbp.local.valenciacc.edu>
2017-08-03 16:58:23 -0400

    photos added

--- server.js
+++ server.js
@@ -8,22 +8,22 @@ var port = process.env.PORT || 3000;
 var friends = [
     {
          name: "Manuela",
-         photo: "http://i.imgur.com/efd8cya.jpg",
+         photo: "https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/1280/720/100/wdpromedia.disney.go.com/media/disneyparks_v0100/1/media/special-offers/special-offers-dlr-generic-pep.jpg",
          survey: [5, 3, 1, 2, 5, 4, 2, 1, 5, 1]
      },
      {
          name: "Karina",
-         photo: "http://i.imgur.com/efd8cya.jpg",
+         photo: "http://img4.zergnet.com/1462067_300.jpg",
          survey: [3, 3, 2, 2, 5, 3, 2, 1, 1, 2]
      },
      {
          name: "Byron",
-         photo: "http://i.imgur.com/efd8cya.jpg",
+         photo: "https://cdn-live-s3.sidearmstreaming.com/allaccess-vod/ugeorgia/12151309.jpeg",
          survey: [5, 3, 3, 4, 2, 1, 3, 4, 1, 5]
      },
      {
          name: "Christy",
-         photo: "http://i.imgur.com/efd8cya.jpg",
+         photo: "http://seaotters.com/wp-content/uploads/2012/03/628x353-otter-cu-yawn.jpg",
          survey: [5, 1, 4, 2, 3, 3, 2, 1, 5, 3]
      }
 ];
